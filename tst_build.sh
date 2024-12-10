d2a23279c799770721ab14cc0027170e01fad927
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2024-12-10 06:50:27 +0100

    202412

--- tst_build.sh
+++ tst_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t npeca75/php:81 -f Dockerfile81 .
-docker push npeca75/php:81
+#docker build -t npeca75/php:81 -f Dockerfile81 .
+#docker push npeca75/php:81
 docker build -t npeca75/php:82 -f Dockerfile .
 docker push npeca75/php:82
 docker image prune -a -f
